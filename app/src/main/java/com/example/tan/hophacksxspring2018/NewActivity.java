3966b880f64e51685858a5926e56ae9a727f0fbf
xli98 <theoria9898@hotmail.com>
2018-02-18 01:27:52 -0500

    Merged camera and app infrastructure, used genericfileprovider to avoid conflict

---
+++ app/src/main/java/com/example/tan/hophacksxspring2018/NewActivity.java
@@ -0,0 +1,195 @@
+package com.example.tan.hophacksxspring2018;
+        import android.content.Context;
+        import android.content.Intent;
+        import android.content.SharedPreferences;
+        import android.graphics.Bitmap;
+        import android.graphics.BitmapFactory;
+        import android.net.Uri;
+        import android.os.Environment;
+        import android.provider.MediaStore;
+        import android.support.v4.content.FileProvider;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.AdapterView;
+        import android.widget.BaseAdapter;
+        import android.widget.Button;
+        import android.widget.GridView;
+        import android.widget.ImageView;
+        import android.widget.Toast;
+
+        import java.io.File;
+        import java.io.IOException;
+        import java.text.SimpleDateFormat;
+        import java.util.ArrayList;
+        import java.util.Date;
+        import java.util.List;
+
+public class NewActivity extends AppCompatActivity {
+
+    private List<String> myList;
+    private GridView gridview;
+    private String mCurrentPhotoPath;
+    public static final String PREFS_NAME = "MyPrefsFile";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        myList = new ArrayList<String>();
+        myList.clear();
+        int size = prefs.getInt("myList.size()", 0);
+
+        for (int i=0; i < size; i++) {
+            String imagePath = prefs.getString(String.format("myList[%d]", i), "");
+        }
+
+        final Context con = this;
+        Button camera = findViewById(R.id.cameraButton);
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                dispatchTakePictureIntent();
+            }
+        });
+
+        gridview = (GridView) findViewById(R.id.gridview1);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+            {
+                // Send File Path to Image Display Activity
+                Intent intent = new Intent(getApplicationContext(), ImageDisplayActivity.class);
+                intent.putExtra("path", myList.get(position));
+                startActivity(intent);
+            }
+        });
+
+
+
+
+    }
+
+
+    //taking a photo with app
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photo = null;
+            try {
+                photo = createImageFile();
+            }
+            catch (IOException ex) {
+                Toast.makeText(this, "You wish, don'cha?", Toast.LENGTH_SHORT).show();
+            }
+
+            if(photo != null){
+                takePictureIntent.putExtra(MediaStore. EXTRA_OUTPUT,
+                        FileProvider.getUriForFile(this, this.getApplicationContext().getPackageName() + ".my.package.name.provider", photo));
+
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+
+        } else {
+            Toast.makeText(this, "Whoops", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String timeS = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String picName = "JPEG_" + timeS + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        File image = File.createTempFile(picName, ".jpg", storageDir);
+
+        mCurrentPhotoPath = image.getAbsolutePath();
+
+        return image;
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //ImageView mImageView = findViewById(R.id.image1);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Intent medIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE );
+            File f = new File(mCurrentPhotoPath );
+            Uri contentUri = Uri.fromFile(f);
+            medIntent.setData(contentUri);
+            this.sendBroadcast(medIntent);
+
+            myList.add(mCurrentPhotoPath);
+            gridview.invalidateViews();
+           /* Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+           // mImageView.setImageBitmap(imageBitmap);*/
+        }
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+
+        editor.putInt("myList.size()", myList.size());
+
+        for (int i=0; i < myList.size(); i++) {
+            editor.putString(String.format("myList[%d]", i), myList.get(i));
+        }
+
+        editor.commit();
+
+
+    }
+
+    public class ImageAdapter extends BaseAdapter {
+        private Context mContext;
+
+        public ImageAdapter(Context context) {
+            mContext = context;
+        }
+
+        public int getCount(){
+            return myList.size();
+        }
+
+        public Object getItem(int position) {
+            return null;
+        }
+
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+
+            if (convertView == null) {
+                imageView = new ImageView(mContext);
+                imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setPadding(8, 8, 8, 8);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+
+            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+            bmOptions.inJustDecodeBounds = false ;
+            bmOptions.inSampleSize = 4;
+            bmOptions.inPurgeable = true ;
+            Bitmap bitmap = BitmapFactory.decodeFile(myList.get(position), bmOptions);
+
+            imageView.setImageBitmap(bitmap);
+
+            return imageView;
+        }
+    }
+}
