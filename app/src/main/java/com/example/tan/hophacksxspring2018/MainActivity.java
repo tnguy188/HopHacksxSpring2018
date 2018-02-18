73fe45121c413aa0ecb3b434054b23843324897a
xli98 <theoria9898@hotmail.com>
2018-02-17 19:12:53 -0500

    Implemented basic app infrastructure and layout

--- app/src/main/java/com/example/tan/hophacksxspring2018/MainActivity.java
+++ app/src/main/java/com/example/tan/hophacksxspring2018/MainActivity.java
@@ -7,42 +7,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //TODO: back navigation at some point
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button camera = findViewById(R.id.cameraButton);
-        camera.setOnClickListener(new View.OnClickListener() {
+        Button saveda = findViewById(R.id.savedact);
+        saveda.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                dispatchTakePictureIntent();
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SavedActivity.class);
+                startActivity(intent);
             }
         });
+
+        Button newa = findViewById(R.id.newact);
+        newa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), NewActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 
-    //taking a photo with app
-    static final int REQUEST_IMAGE_CAPTURE = 1;
 
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        ImageView mImageView = findViewById(R.id.image1);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            mImageView.setImageBitmap(imageBitmap);
-        }
-    }
 }
