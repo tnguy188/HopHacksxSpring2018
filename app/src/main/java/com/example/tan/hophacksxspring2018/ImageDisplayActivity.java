2d9ec725e21b8d8b3a718a36d65aacc618bc7065
xli98 <theoria9898@hotmail.com>
2018-02-18 01:44:48 -0500

    Allows to select file and enlarge it

---
+++ app/src/main/java/com/example/tan/hophacksxspring2018/ImageDisplayActivity.java
@@ -0,0 +1,34 @@
+package com.example.tan.hophacksxspring2018;
+
+import android.app.ActionBar;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+
+public class ImageDisplayActivity extends AppCompatActivity {
+
+    private String path;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_display);
+        Toast.makeText(this, "Yay!", Toast.LENGTH_SHORT).show();
+
+
+        path = getIntent().getExtras().getString("path");
+        ImageView imageView = (ImageView)findViewById(R.id.bigimage);
+
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions. inJustDecodeBounds = false;
+        bmOptions. inSampleSize = 4;
+        bmOptions. inPurgeable = true ;
+        Bitmap bitmap = BitmapFactory.decodeFile(path, bmOptions);
+
+        // Display Image
+        imageView.setImageBitmap(bitmap);
+    }
+}
